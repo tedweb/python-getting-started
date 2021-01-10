a8c7cc83cb972ebba2dc202702e237b16382bace
Ted <mrtedweb@gmail.com>
2021-01-10 01:19:07 -0500

    Test

--- hello/views.py
+++ hello/views.py
@@ -8,14 +8,14 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    #return render(request, "index_ted.html")
+    return render(request, "index_ted.html")
 
     #r = requests.get('http://httpbin.org/status/418')
     #print(r.text)
     #return HttpResponse('<pre>' + r.text + '</pre>')
 
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+    #times = int(os.environ.get('TIMES',3))
+    #return HttpResponse('Hello! ' * times)
 
 
 def db(request):
